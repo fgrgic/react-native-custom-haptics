f85ea5dd971108a48c3a882fd881e4887f5fcc44
Fran Grgić <grgicfran@gmail.com>
2022-05-28 17:17:08 +0200

    :spark: HapticsProvider working

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
 import Main from './Main';
-// import { HapticsProvider } from 'react-native-custom-haptics';
+import { HapticsProvider } from 'react-native-custom-haptics';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -12,9 +12,9 @@ export default function App() {
   // }, []);
 
   return (
-    // <HapticsProvider>
-    <Main />
-    // </HapticsProvider>
+    <HapticsProvider>
+      <Main />
+    </HapticsProvider>
   );
 }
 
--- example/src/Main/Main.tsx
+++ example/src/Main/Main.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Button, View } from 'react-native';
-// import { HapticsProvider, useHaptics } from 'react-native-custom-haptics';
-import HapticsProvider from 'react-native-custom-haptics';
+import { Button, View, Text } from 'react-native';
+import { HapticsProvider, useHaptics } from 'react-native-custom-haptics';
+// import HapticsProvider from 'react-native-custom-haptics';
 
 interface IMain {}
 
 const Main = (props: IMain) => {
+  const { run, stop, isRunning } = useHaptics();
   return (
     <View>
-      <Button title="Press meeee" />
-      <HapticsProvider />
+      <Text>ftroafjds</Text>
+      <Text>ftroafjds</Text>
+      <Text>ftroafjds</Text>
+      <Text>ftroafjds</Text>
+      <Text>ftroafjds</Text>
+      <Button
+        title="Press meeee"
+        onPress={() => {
+          if (isRunning) stop();
+          run('light', 300, 'heavy');
+        }}
+      />
     </View>
   );
 };
